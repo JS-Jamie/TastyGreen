18fd68a8ddfbc8f72a091b34a66c65e40292903c
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-08-01 22:27:51 -0700

    Initial commit

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>TastyGreen</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='/cart'>
+                <i className='fas fa-shopping-cart'></i> Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user'></i> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
