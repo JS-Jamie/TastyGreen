8c778824a9f5a035566c85aacbe0765785c53a4b
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-08-10 15:08:49 -0700

    Data Models created

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,9 +6,11 @@ const connectDB = async () => {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    //the error below is for when it can't connect correctly (when 'npm run server' fails).
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
   }
 };
