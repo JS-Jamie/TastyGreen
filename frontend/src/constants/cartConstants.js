e95064e5b29105a9220e59264a3770dc7d5c6f64
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-09-01 15:07:28 -0700

    Remove items from cart

--- frontend/src/constants/cartConstants.js
+++ frontend/src/constants/cartConstants.js
@@ -1,2 +1,6 @@
 export const CART_ADD_ITEM = 'CART_ADD_ITEM';
 export const CART_REMOVE_ITEM = 'CART_REMOVE_ITEM';
+
+//For CART_ADD_ITEM, the order is constants-reducers-store-actions-cartscreen
+//For CART_REMOVE_ITEM, the order is
+//constants - reducers - actions - cartScreen
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -11,7 +11,7 @@ import {
   Card,
 } from 'react-bootstrap';
 import Message from '../components/Message';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 
 const CartScreen = () => {
   const params = useParams();
@@ -35,7 +35,7 @@ const CartScreen = () => {
   }, [dispatch, productId, qty]);
 
   const removeFromCartHandler = (id) => {
-    console.log('remove');
+    dispatch(removeFromCart(id));
   };
 
   const checkoutHandler = () => {
