65ee62a5f1ccbcc02e82e058dee6bb445321ca01
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-09-12 12:57:34 -0700

    Add uer register reducer, action, and screen

--- frontend/src/constants/cartConstants.js
+++ frontend/src/constants/cartConstants.js
@@ -1,6 +1,6 @@
 export const CART_ADD_ITEM = 'CART_ADD_ITEM';
 export const CART_REMOVE_ITEM = 'CART_REMOVE_ITEM';
 
-//For CART_ADD_ITEM, the order is constants-reducers-store-actions-cartscreen
+//For CART_ADD_ITEM, the order is constants-reducers-store-actions-cartscreen-app.js(to add the screen that's just been created)
 //For CART_REMOVE_ITEM, the order is
 //constants - reducers - actions - cartScreen
