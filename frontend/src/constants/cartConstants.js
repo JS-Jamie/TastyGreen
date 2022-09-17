40f09999e812b99ebd11a7c18ec4a79186ceff53
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-09-17 14:42:11 -0700

    Add shipping screen

--- frontend/src/constants/cartConstants.js
+++ frontend/src/constants/cartConstants.js
@@ -1,5 +1,6 @@
 export const CART_ADD_ITEM = 'CART_ADD_ITEM';
 export const CART_REMOVE_ITEM = 'CART_REMOVE_ITEM';
+export const CART_SAVE_SHIPPING_ADDRESS = 'CART_SAVE_SHIPPING_ADDRESS';
 
 //For CART_ADD_ITEM, the order is constants-reducers-store-actions-cartscreen-app.js(to add the screen that's just been created)
 //For CART_REMOVE_ITEM, the order is
