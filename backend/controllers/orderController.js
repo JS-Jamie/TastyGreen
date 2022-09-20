28c58d9ba15bdf67f4e49857b78548ff17dd0135
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-09-20 13:38:19 -0700

    Update back-end files to update order to paid

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -54,4 +54,30 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+//@desc    Update order to "Paid"
+//@route    GET /api/orders/:id/pay
+//@access    Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true; //it's false by default. so need to set it to true.
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    }; // all of these above will come from the PayPal response
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
+
+//update Routes after this
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,15 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems, getOrderById } from '../controllers/orderController.js';
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, getOrderById);
 // Make sure this param with the id (the one right above) is at the bottom
+router.route('/:id/pay').put(protect, updateOrderToPaid);
+
 export default router;
