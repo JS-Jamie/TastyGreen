7457856c1377b0bee0505653ac998ba9eef23564
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-09-20 10:51:42 -0700

    Update backend orderController.js and orderRoutes.js to Get order by ID endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems } from '../controllers/orderController.js';
+import { addOrderItems, getOrderById } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(protect, addOrderItems);
-
+router.route('/:id').get(protect, getOrderById);
+// Make sure this param with the id (the one right above) is at the bottom
 export default router;
