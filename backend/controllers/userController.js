34ce902af78bd7ebad250da727f2f68444c303a4
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-09-26 11:37:19 -0700

    Add Admin middleware and Get Users endpoint in back end files

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -102,4 +102,14 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser, getUserProfile, updateUserProfile };
+//@desc    Get all users
+//@route    GET /api/users
+//@access    Private/Admin (have to login as an admin)
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers };
+
+//After this, update routes.
