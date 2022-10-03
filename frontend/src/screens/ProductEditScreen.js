3319b245863f60ab3a0be4aa3c1c44910787ec47
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-10-02 23:04:32 -0700

    Add product edit screen. Admin only

---
+++ frontend/src/screens/ProductEditScreen.js
@@ -0,0 +1,98 @@
+// copied the entire codes from the UserEditScreen.js as they are very similar.
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import FormContainer from '../components/FormContainer';
+import { listProductDetails } from '../actions/userActions';
+
+const ProductEditScreen = () => {
+  const navigate = useNavigate();
+  const params = useParams();
+  const productId = params.id;
+
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState(0);
+  const [image, setImage] = useState('');
+  const [countInStock, setCountInStock] = useState(0);
+  const [descripion, setDescription] = useState('');
+
+  const dispatch = useDispatch();
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    if (!product.name || product._id !== productId) {
+      dispatch(listProductDetails(productId));
+    } else {
+      setName(product.name);
+      setPrice(product.price);
+      setImage(product.image);
+      setCountInStock(product.countInStock);
+      setDescription(product.descripion);
+    }
+  }, [dispatch, navigate, productId, product]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // UPDATE PRODUCT
+  };
+
+  return (
+    <>
+      <Link to='/admin/userlist' className='btn btn-light my-3'>
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit User</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='danger'>{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId='name'>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type='name'
+                placeholder='Enter name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='email'>
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Enter email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='isadmin'>
+              <Form.Check
+                type='checkbox'
+                label='Is Admin'
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.checked)}
+              ></Form.Check>
+            </Form.Group>
+
+            <Button type='submit' variant='primary'>
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  );
+};
+
+export default ProductEditScreen;
