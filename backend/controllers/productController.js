dc841242d482ad72677b4d4aa31fa7de2fd0e14b
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-10-05 13:01:43 -0700

    Add front-end product reviews

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -91,7 +91,7 @@ const createProductReview = asyncHandler(async (req, res) => {
 
   if (product) {
     const alreadyReviewed = product.reviews.find(
-      (r) => r.user.toString() === req.user._id.toStrong()
+      (r) => r.user.toString() === req.user._id.toString()
     );
     if (alreadyReviewed) {
       res.status(404);
