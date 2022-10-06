8768b1dfa5125d5a604e2dc108cb9d894c334554
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-10-06 13:29:31 -0700

    Add custom page titles and meta

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -13,6 +13,7 @@ import {
 import Rating from '../components/Rating';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
+import Meta from '../components/Meta';
 import {
   listProductDetails,
   createProductReview,
@@ -73,6 +74,7 @@ const ProductScreen = () => {
         <Message variant='danger'>{error}</Message>
       ) : (
         <>
+          <Meta title={product.name} />
           <Row>
             <Col md={4}>
               <Image src={product.image} alt={product.name} fluid />
